c86e8dc9a7b928079ca4fcbe2d0de1a1e607a24d
Jacqueline Lee <anginblu@gmail.com>
2017-12-28 06:37:13 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.string :num_of_employees
       t.boolean :still_in_business
-      t.text :opening_time
+      t.datetime :opening_time
       t.text :closing_time
     end
   end
